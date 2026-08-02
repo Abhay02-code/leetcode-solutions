c5fc9b8d98fc69ee26712f3be902d04ce717fb15
Abhay02-code <abhayaditya305@gmail.com>
2026-08-02 19:39:22 +0530

    Add solution for Add Digits

---
+++ LeetCode/Add-Digits/Solution.cpp
@@ -0,0 +1,18 @@
+1class Solution {
+2public:
+3    int addDigits(int num) {
+4
+5        while (num >= 10) {
+6            int sum = 0;
+7
+8            while (num > 0) {
+9                sum += num % 10;
+10                num /= 10;
+11            }
+12
+13            num = sum;
+14        }
+15
+16        return num;
+17    }
+18};
