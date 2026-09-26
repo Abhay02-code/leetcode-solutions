b22566f86ddfd5e0c5d10432a24cfd1444facaca
Abhay02-code <abhayaditya305@gmail.com>
2026-09-26 14:42:22 +0530

    Add solution for Array With Elements Not Equal to Average of Neighbors

---
+++ LeetCode/Array-With-Elements-Not-Equal-to-Average-of-Neighbors/Solution.cpp
@@ -0,0 +1,11 @@
+    vector<int> v(nums.begin(), nums.end());
+  //  sort(v.begin(), v.end());
+
+    for(int i=0;i<n;i++) {
+        if (i-1>=0 && i+1<n && (v[i-1]+v[i+1])%2==0 && (v[i-1]+v[i+1])/2==v[i]) {
+            swap<int>(v[i], v[i-1]);
+        }
+    }
+
+    return v;
+}
